1e6f0981bcb017fa1649fbfa3f204c65d00d2587
alex <67626131+alexpvpmindustry@users.noreply.github.com>
2021-08-03 15:18:19 +0800

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+import { conjson } from "cnt2.js";
+import { Web3 } from "https://cdn.jsdelivr.net/npm/web3@1.3.4/dist/web3.min.js";
 window.addEventListener("DOMContentLoaded", (event) => {
   // Activate Bootstrap scrollspy on the main nav element
   const mainNav = document.body.querySelector("#mainNav");
@@ -20,4 +22,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
       }
     });
   });
+  console.log("script loa22ded");
+  render();
+  console.log("script fin");
 });
+
+//contractjson
+const render = async () => {
+  const web3 = new Web3("https://bsc-dataseed.binance.org/");
+  const contractABI = conjson;
+  console.log(conjson);
+  let contractAddress = "0x3c37ab18d0ec386d06dd68e3470e49bfdc0d46e8";
+  let contract = new web3.eth.Contract(
+    JSON.parse(contractABI),
+    contractAddress
+  );
+  const currentMessage = await contract.methods
+    .a_public_getContractBalanceWeiTok()
+    .call();
+  console.log(currentMessage.weii);
+};
